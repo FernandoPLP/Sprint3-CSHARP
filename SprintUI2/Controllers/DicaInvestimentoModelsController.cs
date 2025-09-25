5f43815cb105d615e68b80f3769108c357d05bf6
Fernando-Popolili <fpopolili@gmail.com>
2025-09-24 21:47:53 -0300

    Continução projeto com interface web

---
+++ SprintUI2/Controllers/DicaInvestimentoModelsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SprintData;
+using SprintModel;
+
+namespace SprintUI2.Controllers
+{
+    public class DicaInvestimentoModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DicaInvestimentoModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DicaInvestimentoModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dicas.ToListAsync());
+        }
+
+        // GET: DicaInvestimentoModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dicaInvestimentoModel = await _context.Dicas
+                .FirstOrDefaultAsync(m => m.idDica == id);
+            if (dicaInvestimentoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(dicaInvestimentoModel);
+        }
+
+        // GET: DicaInvestimentoModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DicaInvestimentoModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("idDica,titulo,descricao,categoria,dataPublicacao,link")] DicaInvestimentoModel dicaInvestimentoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dicaInvestimentoModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dicaInvestimentoModel);
+        }
+
+        // GET: DicaInvestimentoModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dicaInvestimentoModel = await _context.Dicas.FindAsync(id);
+            if (dicaInvestimentoModel == null)
+            {
+                return NotFound();
+            }
+            return View(dicaInvestimentoModel);
+        }
+
+        // POST: DicaInvestimentoModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("idDica,titulo,descricao,categoria,dataPublicacao,link")] DicaInvestimentoModel dicaInvestimentoModel)
+        {
+            if (id != dicaInvestimentoModel.idDica)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dicaInvestimentoModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DicaInvestimentoModelExists(dicaInvestimentoModel.idDica))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dicaInvestimentoModel);
+        }
+
+        // GET: DicaInvestimentoModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dicaInvestimentoModel = await _context.Dicas
+                .FirstOrDefaultAsync(m => m.idDica == id);
+            if (dicaInvestimentoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(dicaInvestimentoModel);
+        }
+
+        // POST: DicaInvestimentoModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dicaInvestimentoModel = await _context.Dicas.FindAsync(id);
+            if (dicaInvestimentoModel != null)
+            {
+                _context.Dicas.Remove(dicaInvestimentoModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DicaInvestimentoModelExists(int id)
+        {
+            return _context.Dicas.Any(e => e.idDica == id);
+        }
+    }
+}
---
+++ SprintUI2/Controllers/InvestimentoModelsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SprintData;
+using SprintModel;
+
+namespace SprintUI2.Controllers
+{
+    public class InvestimentoModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvestimentoModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: InvestimentoModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Investimentos.ToListAsync());
+        }
+
+        // GET: InvestimentoModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var investimentoModel = await _context.Investimentos
+                .FirstOrDefaultAsync(m => m.idInvestimento == id);
+            if (investimentoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(investimentoModel);
+        }
+
+        // GET: InvestimentoModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InvestimentoModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("idInvestimento,nome,tipo,rentabilidadeAnual,risco,valorMinimo")] InvestimentoModel investimentoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(investimentoModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(investimentoModel);
+        }
+
+        // GET: InvestimentoModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var investimentoModel = await _context.Investimentos.FindAsync(id);
+            if (investimentoModel == null)
+            {
+                return NotFound();
+            }
+            return View(investimentoModel);
+        }
+
+        // POST: InvestimentoModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("idInvestimento,nome,tipo,rentabilidadeAnual,risco,valorMinimo")] InvestimentoModel investimentoModel)
+        {
+            if (id != investimentoModel.idInvestimento)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(investimentoModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvestimentoModelExists(investimentoModel.idInvestimento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(investimentoModel);
+        }
+
+        // GET: InvestimentoModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var investimentoModel = await _context.Investimentos
+                .FirstOrDefaultAsync(m => m.idInvestimento == id);
+            if (investimentoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(investimentoModel);
+        }
+
+        // POST: InvestimentoModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var investimentoModel = await _context.Investimentos.FindAsync(id);
+            if (investimentoModel != null)
+            {
+                _context.Investimentos.Remove(investimentoModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvestimentoModelExists(int id)
+        {
+            return _context.Investimentos.Any(e => e.idInvestimento == id);
+        }
+    }
+}
